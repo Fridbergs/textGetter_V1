bfe366c9b84f1091f3e5a8d4df8b4aa0ebe0cfcf
Linus Fridberg <linus@fridbergs.com>
2024-01-10 21:30:18 +0100

    added csv download link

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   displaySavedData();
   document.getElementById("getDataButton").addEventListener("click", getData);
   document.getElementById("savedTable").addEventListener("click", copyData); // Attach copyData to the click event of the table
+  document.getElementById("downloadData").addEventListener("click", createCsv); // Add event listener for "Download Data" button
 });
 
 function displaySavedData() {
@@ -44,8 +45,6 @@ function getData() {
   });
 }
 
-// popup.js
-
 function copyData() {
   let savedData = "";
 
@@ -57,4 +56,40 @@ function copyData() {
 
   // Copy the concatenated data to the clipboard
   navigator.clipboard.writeText(savedData);
+}
+
+function createCsv() {
+  var table = document.getElementById("savedTable");
+  var rows = table.getElementsByTagName("tr");
+  var csvContent = [];
+
+  // Add header row
+  var headerRow = [];
+  var headerCells = rows[0].getElementsByTagName("td");
+  for (var k = 0; k < headerCells.length; k++) {
+    headerRow.push("extracted text");
+  }
+  csvContent.push(headerRow.join(","));
+
+  // Add data rows
+  for (var i = 0; i < rows.length; i++) {
+    var rowData = [];
+    var cells = rows[i].getElementsByTagName("td");
+
+    for (var j = 0; j < cells.length; j++) {
+      rowData.push(cells[j].innerText);
+    }
+
+    csvContent.push(rowData.join(","));
+  }
+
+  var csvString = csvContent.join("\n");
+
+  var blob = new Blob([csvString], { type: "text/csv" });
+  var a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "data.csv";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
